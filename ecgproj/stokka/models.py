53f9a2ffc085af4af2a8532a82a578ee3d2177d3
Jabekah007 <70330339+Jabekah007@users.noreply.github.com>
2022-07-18 23:51:04 +0000

    update user model

--- ecgproj/stokka/models.py
+++ ecgproj/stokka/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from datetime import  datetime
 # Create your models here.
 '''
@@ -13,7 +14,17 @@ from datetime import  datetime
 '''                               
 
 
-       
+User = get_user_model()
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+   # Id_user =models.IntegerField()
+    bio =models.TextField(blank=True)
+    location = models.CharField(max_length=100,blank=True) 
+
+    def __str__(self) :
+        return self.user   
+
+
 
 class Product(models.Model):
 
@@ -21,14 +32,19 @@ class Product(models.Model):
     Asset_Name = models.CharField(max_length=15)
     Serial_No =  models.CharField(max_length=15)
     Mac_address = models.CharField(max_length=15)
-    Memmory_Size = models.IntegerField()
-    HHD_Size = models.IntegerField()
+    Memmory_Size = models.IntegerField(default=0)
+    HHD_Size = models.IntegerField(default=0)
     Asset_Model= models.CharField(max_length=15)
+    Asset_Location = models.CharField(max_length=15,blank=True)
     Power_capacity = models.IntegerField(default=0)
     time_created = models.DateTimeField(default=datetime.now)
 
+
+
     def __str__(self) :
-        return self.Asset_Name                    
+        return self.Asset_Location
+
+
 
 
 
