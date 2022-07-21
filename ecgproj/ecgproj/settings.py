92fc7c842ff1247d3fa1b4076e69d318095dd5e0
Jabekah007 <70330339+Jabekah007@users.noreply.github.com>
2022-07-21 22:26:01 +0000

    added sign in page

--- ecgproj/ecgproj/settings.py
+++ ecgproj/ecgproj/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-9kq0i2phrrp!ocrz8*v@0xig9_8$$qx1m6*bll6yw!7c^$y_^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- ecgproj/stokka/views.py
+++ ecgproj/stokka/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from django.shortcuts import redirect,render
 
 # Create your views here.
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials invalid')
+            return redirect('signin')
+    else:
+        return render(request,"signin.html")
